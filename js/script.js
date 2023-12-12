2cb211390108abdba4a1309a9dbcb9c14331578a
BarnabedevWebmobile <barnabe.dwwm48@gmail.com>
2023-12-12 14:59:24 +0100

    modif carte, js
    modification des cartes début de l'inclusion de Js et de l'api

--- js/script.js
+++ js/script.js
@@ -27,19 +27,16 @@ fetch('https://portfolios.ern-mende.fr/wp-json/wp/v2/apprenants')
       var Apprenant1_nom = document.getElementById('nom1');
       
       var textAEcrire  = data[0]["nom"];
-      Apprenant1_nom.innerHTML = "Nom : "+textAEcrire
+      Apprenant1_nom.innerHTML = textAEcrire
       
-      var Apprenant1_prenom = document.getElementById('premon1');
+      var Apprenant1_prenom = document.getElementById('prenom1');
       textAEcrire  = data[0]["prenom"];
-      Apprenant1_prenom.innerHTML = "Prénom :"+textAEcrire
+      Apprenant1_prenom.innerHTML = textAEcrire
 
 
       var Apprenant1_annee = document.getElementById('annee1');
       id_de_annee = data[0]["promotions"][0];
-      trouverAnnee(id_de_annee)
-      
-
-
+      Apprenant1_annee.innerHTML = id_de_annee
       
       var Apprenant1_avatar = document.getElementById('avatar1');
       textAEcrire  = data[0]["image"];
@@ -59,7 +56,46 @@ fetch('https://portfolios.ern-mende.fr/wp-json/wp/v2/apprenants')
 
       var Apprenant1_cv = document.getElementById('cv1');
       textAEcrire  = data[0]["link"];
+      Apprenant1_cv.href = textAEcrire
+      
+      console.log(data)
+      console.log(data[0]["nom"])
+      var Apprenant1_nom = document.getElementById('nom2');
+      
+      var textAEcrire  = data[1]["nom"];
+      Apprenant1_nom.innerHTML = textAEcrire
+      
+      var Apprenant1_prenom = document.getElementById('prenom2');
+      textAEcrire  = data[1]["prenom"];
+      Apprenant1_prenom.innerHTML = textAEcrire
+
+
+      var Apprenant1_annee = document.getElementById('annee2');
+      id_de_annee = data[1]["promotions"][0];
+      Apprenant1_annee.innerHTML = id_de_annee
+      
+      var Apprenant1_avatar = document.getElementById('avatar2');
+      textAEcrire  = data[1]["image"];
+      Apprenant1_avatar.src = textAEcrire
+
+      var Apprenant1_extrait = document.getElementById('extrait2');
+      textAEcrire  = data[1]["excerpt"]["rendered"];
+      Apprenant1_extrait.innerHTML = textAEcrire
+
+      var Apprenant1_portfolio = document.getElementById('portfolio2');
+      textAEcrire  = data[1]["portfolio"];
+      Apprenant1_portfolio.href = textAEcrire
+
+      var Apprenant1_cv = document.getElementById('cv2');
+      textAEcrire  = data[1]["link"];
+      Apprenant1_cv.href = textAEcrire
+
+      var Apprenant1_cv = document.getElementById('cv2');
+      textAEcrire  = data[1]["link"];
       Apprenant1_cv.href = textAEcrire 
     })
+    .catch(error =>{
+      console.log('erreur', error)
+    });
 
     
